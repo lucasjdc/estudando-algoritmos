d8bd8969bd2c881dda2fd8555d62360e44d58d06
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-04-19 20:26:27 -0300

    Calcula á média de 3 notas

---
+++ media.f90
@@ -0,0 +1,13 @@
+program calcula_media
+    implicit none
+    real matematica,ciencias,literatura,media
+
+    matematica = 85
+    ciencias = 90
+    literatura = 88
+
+    media = (matematica + ciencias + literatura) / 3
+
+    print *, "A média é:", media
+
+end program calcula_media
